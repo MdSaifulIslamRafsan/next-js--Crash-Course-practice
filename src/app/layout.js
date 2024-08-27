565988a0b20e241bf683ce54a8ff5082768926ce
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-08-27 23:16:15 +0600

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,8 +10,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="p-6">
+      <body className={inter.className}>
+        <nav className="bg-gray-600 text-center uppercase">home | about</nav>
+        {children}
+        <footer className="bg-gray-600 text-center uppercase">this is footer</footer>
+        </body>
     </html>
   );
 }
