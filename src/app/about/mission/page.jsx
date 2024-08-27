6ff5aca4383f23cb2b28481b1f8c2d8ee10be77a
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-08-28 00:16:09 +0600

    added route and nested route concept

---
+++ src/app/about/mission/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MissionPage = () => {
+    return (
+        <main className='my-10'>
+            <h1>this is mission page</h1>
+        </main>
+    );
+};
+
+export default MissionPage;
---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <main className='my-10'>
+            <h1>This Is about page</h1>
+        </main>
+    );
+};
+
+export default About;
