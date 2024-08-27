e0c226e1ebd83c7a85612efb7529d6fa01a34a13
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-08-28 01:27:11 +0600

    added loading handling code

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -1,9 +1,15 @@
+import Link from 'next/link';
 import React from 'react';
 
 const layout = ({children}) => {
     return (
         <div>
-            <nav>mission || vision</nav>
+            <nav>
+            <ul className="flex justify-center my-5 gap-5">
+            <li><Link href="/about/mission">mission</Link></li>
+            <li><Link href="/about/vision">vision</Link></li>
+          </ul>
+            </nav>
             {children}
         </div>
     );
