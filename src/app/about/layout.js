b3d056b04e4da3e995bafce036bf373242136913
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-08-30 02:18:49 +0600

    added static title

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -1,6 +1,9 @@
 import Link from 'next/link';
 import React from 'react';
-
+export const metadata = {
+    title: "About us",
+    description: "This is a about page",
+  };
 const layout = ({children}) => {
     return (
         <div>
