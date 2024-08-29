ff9857da10bcb9fd3ebdefc48c68c2999453d603
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-08-30 01:54:29 +0600

    added image

--- src/app/about/mission/page.jsx
+++ src/app/about/mission/page.jsx
@@ -1,9 +1,13 @@
+
 import Button from "@/app/component/Button";
+import Image from "next/image";
+import thumb from "@/../public/images/pang-yuhao-oRwZvLYL1Ug-unsplash.jpg"
 
 const MissionPage = () => {
     return (
         <main className='my-10'>
             <h1>this is mission page</h1>
+            <Image placeholder="blur" src={thumb} alt="thumb"></Image>
             <Button></Button>
         </main>
     );
