e0cbac598560e8b40b87fd8681aca11278c28af2
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-08-31 02:22:47 +0600

    added peralal fatch

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import getPost from '../../../../lib/getPost';
+import getPostComment from '../../../../lib/getPostComment';
 
 export async function generateMetadata({params}) {
     const {id} = params;
@@ -11,7 +12,11 @@ export async function generateMetadata({params}) {
 }
 const page = async ({params}) => {
     const {id} = params;
-    const post = await getPost(id);
+    const postPromise =  getPost(id);
+    const commentPromise = getPostComment(id);
+    const [post , comments] = await Promise.all([postPromise , commentPromise])
+console.log(comments);
+
 
     
     
@@ -20,6 +25,7 @@ const page = async ({params}) => {
         <div className='mt-6 space-y-4'>
             <h2 className='text-xl font-bold'>title:- {post?.title}</h2>
             <p>body:- {post?.body}</p>
+            {/* <p>{comments}</p> */}
         </div>
     );
 };
